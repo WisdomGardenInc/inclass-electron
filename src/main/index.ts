744e85e59931a2a0ced01b242954627e1707538e
Lu Di <ludi@wisdomgarden.com>
2022-10-28 12:06:42 +0800

    chore(release): release 0.0.13-bqe-debug-3

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, BrowserView, ipcMain, Menu, session } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu, session } from 'electron'
 import './dialog'
 import { Logger } from './logger'
 import { initialize } from './services'
@@ -27,12 +27,12 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     height: 800,
     width: 1300,
-    webPreferences: {
-      preload: indexPreload,
-      contextIsolation: true,
-      nodeIntegration: false,
-      webSecurity: false
-    },
+    // webPreferences: {
+    //   // preload: indexPreload,
+    //   // contextIsolation: true,
+    //   // nodeIntegration: false,
+    //   // webSecurity: false
+    // },
     icon: logoUrl
   })
 
@@ -59,13 +59,7 @@ function createWindow() {
   })
 
   mainWindow.webContents.openDevTools()
-
-  const view = new BrowserView()
-  mainWindow.setBrowserView(view)
-  view.setBounds({ x: 0, y: 0, width: 800, height: 1300 })
-  view.webContents.loadURL('http://lms.sgxx.cn/inclass/courses')
-
-  // mainWindow.loadURL('http://lms.sgxx.cn/inclass/courses')
+  mainWindow.loadURL('http://lms.sgxx.cn/inclass/courses')
 
   // mainWindow.webContents.on('will-redirect', function (e, newURL, isInPlace, isMainFrame) {
   //   if (isMainFrame) {
@@ -74,23 +68,23 @@ function createWindow() {
   //   }
   // })
 
-  mainWindow.webContents.on('did-create-window', (childWindow) => {
-    // mainWindow.webContents.on('will-redirect', function (e, newURL, isMainFrame) {
-    //   console.log(newURL, isMainFrame)
-    //   if (isMainFrame) {
-    //     setTimeout(() => mainWindow.loadURL(newURL), 100)
-    //     e.preventDefault()
-    //   }
-    // })
-    // childWindow.webContents.on('will-redirect', async (e, url) => {
-    //   if (currentOrg && url.includes('/user/index')) {
-    //     mainWindow.loadURL(`${currentOrg.apiUrl}/inclass/courses`)
-    //     childWindow.close()
-    //     mainWindow.maximize()
-    //     mainWindow.fullScreen = true
-    //   }
-    // })
-  })
+  // mainWindow.webContents.on('did-create-window', (childWindow) => {
+  // mainWindow.webContents.on('will-redirect', function (e, newURL, isMainFrame) {
+  //   console.log(newURL, isMainFrame)
+  //   if (isMainFrame) {
+  //     setTimeout(() => mainWindow.loadURL(newURL), 100)
+  //     e.preventDefault()
+  //   }
+  // })
+  // childWindow.webContents.on('will-redirect', async (e, url) => {
+  //   if (currentOrg && url.includes('/user/index')) {
+  //     mainWindow.loadURL(`${currentOrg.apiUrl}/inclass/courses`)
+  //     childWindow.close()
+  //     mainWindow.maximize()
+  //     mainWindow.fullScreen = true
+  //   }
+  // })
+  // })
 
   return mainWindow
 }
